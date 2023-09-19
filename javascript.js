c9c46f4d6bfcce8cd739f97d4e196599beb810a1
rai <138423936+rei-jo@users.noreply.github.com>
2023-09-19 02:10:03 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,45 @@
+ //menu
+ var tombolMenu = $(".tombol-menu");
+ var menu = $("nav .menu ul");
+
+ function klikmenu(){
+    tombolMenu.click(function(){
+         menu.toggle();
+    });
+    menu.click (function(){
+        menu.toggle();
+    });
+ }
+
+ $(document).ready(function(){
+    var width =$(window).width();
+    if(width < 990){
+        klikmenu();
+    }
+ })
+
+ //check lebar 
+ $(window).resize(function(){
+    var width = $(window).width();
+    if(width > 989){
+menu.css("display", "block");
+     //display:block
+    }else{
+        menu.css("display", "none");
+    }
+    klikmenu();
+ });
+
+ //efek scroll
+ $(document).ready(function(){
+    var scroll_pos = 0;
+    $(document).scroll(function(){
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 0){
+            $("nav").addClass("putih");
+        }else {
+            $("nav").removeClass("putih");
+        }
+    })
+ })
+    
